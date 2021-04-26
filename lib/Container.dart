bc57c994222ba9eae44541d2446258da11d3008b
RuslanProgrammer <55411970+RuslanProgrammer@users.noreply.github.com>
2021-04-26 12:49:51 +0300

    Create "Cup"

--- lib/Container.dart
+++ lib/Container.dart
@@ -1,5 +1,27 @@
-class Container {
+import 'package:water_overflow/Liquid.dart';
+
+abstract class Container {
   double size;
   String name;
   List<Container> usualContainers;
+
+  bool AddLiquid(Liquid liquid);
+}
+
+class Cup implements Container{
+  @override
+  String name;
+
+  @override
+  double size;
+
+  @override
+  List<Container> usualContainers;
+
+  @override
+  bool AddLiquid(Liquid liquid) {
+    // TODO: implement AddLiquid
+    throw UnimplementedError();
+  }
+
 }
