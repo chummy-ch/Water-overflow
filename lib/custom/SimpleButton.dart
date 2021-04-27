7aa7b7a297f653d78a9fad23cb733dd4049c51ef
Ira Kosenkova <Lrinakos2002@gmail.com>
2021-04-27 16:01:21 +0300

    adding flexibility to MainScreen

--- lib/custom/SimpleButton.dart
+++ lib/custom/SimpleButton.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:water_overflow/utils/Constants.dart';
 
-class PanelButton extends StatelessWidget{
+class SimpleButton extends StatelessWidget{
   final Widget child;
   final double height, width;
 
-  const PanelButton({Key key, this.height, this.width,
+  const SimpleButton({Key key, this.height, this.width,
     @required this.child}) : super(key: key);
   @override
   Widget build(BuildContext context){
@@ -16,7 +16,17 @@ class PanelButton extends StatelessWidget{
           color: COLOR_BUTTON,
           boxShadow:[ BoxShadow(offset: const Offset(1.0, 3.0),
             blurRadius: 4.0,
-            spreadRadius: 0.7,)]
+            spreadRadius: 0.7,)],
+        gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+          stops: [0.1, 0.5, 0.9],
+        colors: [
+          COLOR_BUTTON,
+          COLOR_BLUE_WHITE,
+          COLOR_BUTTON,
+        ],
+      ),
       ),
       child: Center(child: child),
     );
