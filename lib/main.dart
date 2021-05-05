87bd906954217e3ec65c322de12f8194978bbf7c
Ira Kosenkova <Lrinakos2002@gmail.com>
2021-05-05 11:48:28 +0300

    resolving conflicts with develop

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,40 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:water_overflow/screens/MainScreen.dart';
 import 'package:water_overflow/utils/Constants.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() async {
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget{
+  final _initialization  = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          scaffoldBackgroundColor: COLOR_BACKGROUND,
-          textTheme: TEXT_THEME,
-          fontFamily: "SairaCondensed-Light"),
-      home: MainScreen(),
+      theme: ThemeData(scaffoldBackgroundColor: COLOR_BACKGROUND , textTheme: TEXT_THEME, fontFamily: "SairaCondensed-Light" ),
+      home:FutureBuilder(
+        future: _initialization ,
+        builder: (context, snapshot){
+          if(snapshot.hasError){
+            print('ERROR! ${snapshot.error.toString()}');
+            return Text('Something wrong!');
+          }
+          else if(snapshot.hasData){
+            print('All correct');
+            return MainScreen();
+          }
+          else{
+            return Center(child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
     );
   }
 }
